53e224ae32bb1183d3ec7f63ba813a73f44e6666
谢照东 <837199685@qq.com>
2014-11-14 16:56:10 +0800

    增加产生模板工厂的工厂方法

--- src/org/majorxie/dreamvc/helpers/FactoryHelper.java
+++ src/org/majorxie/dreamvc/helpers/FactoryHelper.java
@@ -1,7 +1,9 @@
 package org.majorxie.dreamvc.helpers;
 
 import org.majorxie.dreamvc.exception.NoIocInstanceException;
+import org.majorxie.dreamvc.exception.NoTemplateFactory;
 import org.majorxie.dreamvc.ioc.factory.IocFactory;
+import org.majorxie.dreamvc.template.TemplateFactory;
 
 /**
  * 单例模式,产生对应的容器
@@ -13,7 +15,7 @@ public class FactoryHelper {
 	private FactoryHelper() {
 	}
 
-	private static class SingletonClassInstance {
+	private static class SingletonClassInstance {S
 		private static final FactoryHelper instance = new FactoryHelper();
 	}
 
@@ -38,17 +40,44 @@ public class FactoryHelper {
 		} catch (Exception e) {
 			throw new NoIocInstanceException("No IOC container in your project,check your ioc class file url");
 		}
-		
+		//不属于子类(是否能够强制转换)
 		Object obj = clazz.newInstance();
 		if (obj instanceof IocFactory) {
 			iocFactory = (IocFactory) obj;
 		}
-		//不属于子类
-		if (iocFactory == null||iocFactory.equals("")) {
-			throw new NoIocInstanceException("your class must be a IocFactory class");
+		
+		if (iocFactory == null) {
+			throw new NoIocInstanceException("Your class must be a subclass of IocFactory");
 		}
 
 		return iocFactory;
+	}
+	
+	
+	/**
+	 * 创建模板工厂
+	 * @param clazzName 类全包名
+	 * @return
+	 */
+	public static TemplateFactory createTemplateFactory(String clazzName)throws Exception{
+		TemplateFactory templateFactory=null;
+		Class clazz=null;
+		try {
+			clazz= Class.forName(clazzName);
+		} catch (Exception e) {
+			throw new NoTemplateFactory("Your template factory class not found");
+		}
+		
+		Object obj = clazz.newInstance();
+		if (obj instanceof TemplateFactory) {
+			templateFactory = (TemplateFactory) obj;
+		}
+		if (templateFactory == null) {
+			throw new NoIocInstanceException("Your class must be a subclass of TemplateFactory");
+		}
+			
+		
+		return templateFactory;
 	}
 
 }
