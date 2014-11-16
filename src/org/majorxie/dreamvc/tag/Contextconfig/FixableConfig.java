0889843506efd0b4ca2272c8ce0ebf1c67dd1e26
谢照东 <837199685@qq.com>
2014-11-16 14:51:37 +0800

    修改成策略模式

--- src/org/majorxie/dreamvc/tag/Contextconfig/FixableConfig.java
+++ src/org/majorxie/dreamvc/tag/Contextconfig/FixableConfig.java
@@ -1,10 +1,13 @@
 package org.majorxie.dreamvc.tag.Contextconfig;
+
+import org.majorxie.dreamvc.tag.Contextconfig.StrategyConfig;
+
 /**
  * 2014-10-28
  * @author xiezhaodong
  *
  */
-public interface FixableConfig extends Config {
+public interface FixableConfig extends StrategyConfig {
 	
 	/**
 	 * 得到所有的参数
