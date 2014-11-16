019fb33684a371a7ebcb55213df4118ceca1b131
谢照东 <837199685@qq.com>
2014-11-16 14:52:28 +0800

    修改成策略模式

--- src/org/majorxie/dreamvc/template/JspTemplateFactory.java
+++ src/org/majorxie/dreamvc/template/JspTemplateFactory.java
@@ -2,12 +2,12 @@ package org.majorxie.dreamvc.template;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.majorxie.dreamvc.tag.Contextconfig.Config;
+import org.majorxie.dreamvc.tag.Contextconfig.StrategyConfig;
 
 public class JspTemplateFactory extends TemplateFactory {
 	private final Log log=LogFactory.getLog(getClass());
 	@Override
-	public void init(Config config) {
+	public void init(StrategyConfig config) {
 		log.info("default init jsp template ,if you want to implements others,you can do something from doc");
 	}
 
